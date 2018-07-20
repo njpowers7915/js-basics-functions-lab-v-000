1faf0d93d05aedb552e52d5b844d9269f1981295
Nicholas Powers <njpowers7915@gmail.com>
2018-07-20 01:26:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+  return blocks - 42;
+}
+
+function distanceFromHqInFeet(feet) {
+  return
+}
+
+function distanceTravelledInFeet(feet) {
+  return blocks ;
+}
+
+function calculatesFarePrice(start, destination) {}
