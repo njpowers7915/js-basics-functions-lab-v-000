ab45a8757a8a34ef13e9d544d871484615485333
Nicholas Powers <njpowers7915@gmail.com>
2018-07-20 01:43:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,13 @@ function distanceTravelledInFeet(start, destination) {
 }
 
 function calculatesFarePrice(start, destination) {
-  if (distanceTravelledInFeet(start, destination) > 400 && (distanceTravelledInFeet(start, destination) <= 2000) {
+  if (distanceTravelledInFeet(start, destination) > 400 && distanceTravelledInFeet(start, destination) <= 2000) {
     return ((distanceTravelledInFeet(start, destination) - 400) * .02);
+  }
+  else if (distanceTravelledInFeet(start, destination) > 2000 && distanceTravelledInFeet(start, destination) <= 2500) {
+    return 25;
+  }
+  else {
+    return "cannot travel that far";
   }
 }
